7d3084b0ecf849f8e7e4d6a4d88641defe7dff07
roni702 <152854987+roni702@users.noreply.github.com>
2024-09-16 14:43:46 +0200

    Update

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const typewriterElements = document.querySelectorAll('.typewriter');
+    let totalDelay = 0;
+
+    typewriterElements.forEach((element) => {
+        const text = element.getAttribute('data-text');
+        let index = 0;
+
+        function typeEffect() {
+            if (index < text.length) {
+                element.innerHTML += text.charAt(index);
+                index++;
+                setTimeout(typeEffect, 40); // Snelheid van het typen (100ms)
+            }
+        }
+
+        // Start het type-effect na de totale vertraging
+        setTimeout(typeEffect, totalDelay);
+
+        // Verhoog de totale vertraging voor het volgende element
+        totalDelay += text.length * 100; // Aantal karakters * snelheid van typen
+    });
+});
